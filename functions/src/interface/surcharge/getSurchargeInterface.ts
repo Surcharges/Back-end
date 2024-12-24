195b7b4bf03c8848ab02315598d81372130b2849
Petr <petr.utkin2022@gmail.com>
2024-12-24 15:55:14 +1300

    getSurcharge domain interface created and exported

--- functions/src/interface/surcharge/getSurchargeInterface.ts
+++ functions/src/interface/surcharge/getSurchargeInterface.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { getSurchargeUsecase } from "@domain/surcharge"
 
-interface Response extends express.Response {
+export const getSurchargeInterface = async (req: express.Request, res: express.Response) => {
 
-}
+  try {
+    const placeId  = req.query.placeId as string
+    if (!placeId) {
+      res.status(400).send({ message: "placeId is required" });
+      return;
+    }
+    const surcharge = await getSurchargeUsecase({ id: placeId })
+    res.status(200).send(surcharge);
+  } catch (error) {
+    console.log("Error in getSurchargeInterface:", error);
+    res.status(500).send(error);
+    return
+  }
 
-export const getSurcharge = async (req: express.Request, res: Response) => {
-  res.status(200).send({message: "Hello from getSurcharge!"});
 };
