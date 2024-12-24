42bbf97c0b664aaf002ebf02d17f337d6cdad07d
Petr <petr.utkin2022@gmail.com>
2024-12-24 15:31:07 +1300

    DTOs and repository exported

---
+++ functions/src/data/surcharge/index.ts
@@ -0,0 +1,4 @@
+export { GetSurchargeRepo } from "./repository/getSurchargeRepository"
+export { SurchargeDTO } from "./DTO/SurchargeDTO"
+export { ImageDataDTO } from "./DTO/imageDataDTO"
+export { ResponseDTO } from "./DTO/ResponseDTO"
