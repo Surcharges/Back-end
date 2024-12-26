1271e4935385879a974cdc11a523f5dba7d939c9
PetruccioU <petr.utkin2022@gmail.com>
2024-12-26 14:56:02 +1300

    surcharge post api endpoint added to index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,26 +4,26 @@ import { onRequest } from "firebase-functions/v2/https";
 import express from "express";
 import admin from "firebase-admin";
 import { places, place } from "@interface/place";
-import { getSurchargeInterface } from "@interface/surcharge"
+import { getSurchargeInterface, postSurchargeInterface } from "@interface/surcharge";
+
 
 admin.initializeApp();
 export const db = admin.firestore();
 
 const api = express();
 
-if (process.env.NODE_ENV === 'production') {
-  const cors = require('cors')
+const cors = require('cors')
 
-  const corsOptions = {
-    origin: 'http://bonsung.me',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }
-
-  api.use(cors(corsOptions))
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }
+
+api.use(cors(corsOptions))
 
 api.get("/places", places);
 api.get("/place", place);
 api.get("/api/surcharge", getSurchargeInterface)
+api.post("/api/surcharge", postSurchargeInterface)
 
 exports.api = onRequest(api)
