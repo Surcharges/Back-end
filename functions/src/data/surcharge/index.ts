063a3bb6a74926b9a0ec7e15ac9c11510d75bc61
PetruccioU <petr.utkin2022@gmail.com>
2025-01-03 16:29:48 +1300

    GetSurchargeRepositoryFromGetPlaces exported

--- functions/src/data/surcharge/index.ts
+++ functions/src/data/surcharge/index.ts
@@ -1,4 +1,6 @@
 export { GetSurchargeRepo } from "./get/getSurchargeRepository"
 export { PostSurchargeRepo } from "./post/postSurchargeRepository"
+export { GetSurchargeRepositoryFromGetPlaces } from "./get/getSurchargeRepositoryFromGetPlaces"
 export { GetSurchargeRepositoryResponse } from './get/DTO/GetSurchargeRepositoryResponse'
 export { PostSurchargeRepositoryRequest } from './post/DTO/PostSurchargeRepositoryRequest'
+export { GetSurchargeRepositoryFromGetPlacesResponse } from './get/DTO/GetSurchargeRepositoryFromGetPlacesResponse'
