8a46e6cff90e0e0ac13543c2a7a8aaa1b00ce04b
PetruccioU <petr.utkin2022@gmail.com>
2025-01-05 22:21:01 +1300

    "Fix: Address review comments" PostPlaceRepository introduced

---
+++ functions/src/data/place/post/place/DTO/PostPlaceRepositoryRequest.ts
@@ -0,0 +1,23 @@
+interface LatLngDTO {
+  latitude: number,
+  longitude: number
+}
+
+interface displayName {
+  text: string,
+  languageCode: string
+}
+
+interface AddressComponentsDTO {
+  longText: string,
+  shortText: string,
+  types: string[],
+  languageCode: string
+}
+
+export interface PostPlaceRepositoryRequest {
+  id: string;
+  displayName: displayName,
+  addressComponents: AddressComponentsDTO[],
+  location: LatLngDTO
+}
