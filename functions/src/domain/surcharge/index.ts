86c83454f53943789bebb4d7220d92a1506fc6d1
PetruccioU <petr.utkin2022@gmail.com>
2025-01-15 16:23:39 +1300

    domain index updated

--- functions/src/domain/surcharge/index.ts
+++ functions/src/domain/surcharge/index.ts
@@ -3,4 +3,4 @@ export { postSurchargeUsecase } from "./post/postSurchargeUsecase"
 export { putSurchargeUsecase } from "./put/putSurchargeUsecase"
 export { PostSurchargeUsecaseRequest } from './post/entity/PostSurchargeUsecaseRequest'
 export { PutSurchargeUsecaseRequest } from "./put/entity/PutSurchargeUsecaseRequest"
-export { SurhcargeStatus } from './put/entity/SurhcargeStatus'
+export { SurchargeStatus } from './put/entity/SurchargeStatus'
