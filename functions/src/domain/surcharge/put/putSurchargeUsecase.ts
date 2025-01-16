084b03949d12f398f0d9eb745d867d7381bbfa14
PetruccioU <petr.utkin2022@gmail.com>
2025-01-16 19:16:52 +1300

    putSurchargeUsecase updated to recieve totalAmount and surchargeAmount together

--- functions/src/domain/surcharge/put/putSurchargeUsecase.ts
+++ functions/src/domain/surcharge/put/putSurchargeUsecase.ts
@@ -4,27 +4,59 @@ import { PutSurchargeRepository } from "@data/surcharge"
 import { SurchargeStatus } from "./entity/SurchargeStatus"
 import { rateCalculatorHelper } from "../helpers/rateCalculatorHelper";
 
-export const putSurchargeUsecase = async(request: PutSurchargeUsecaseRequest): Promise<any> => {
-    // const surchargeToUpdate = await GetSurchargesRepository(request.id)
-    let newRate = undefined
-    if (request.totalAmount && request.surchargeAmount){
-        newRate = rateCalculatorHelper(
-            undefined,
-            request.totalAmount ?? 0, // Default to 0 if undefined
-            request.surchargeAmount ?? 0 // Default to 0 if undefined
-          )
-    } else { newRate = undefined }
-    
-    const updatedSurcharge = {
-        id: request.id,
-        surchargeAmount: request.surchargeAmount ?? undefined,
-        totalAmount: request.totalAmount ?? undefined,
-        rate: newRate,
-        surchargeStatus: SurchargeStatus.CONFIRMED
-    }
-    console.log("putSurchargeUsecase surchargeAmount and totalAmount : " 
-        + updatedSurcharge.surchargeAmount, updatedSurcharge.totalAmount)
-    await PutSurchargeRepository(updatedSurcharge)
-    return updatedSurcharge
+export const putSurchargeUsecase = async (
+  request: PutSurchargeUsecaseRequest
+): Promise<{ id: string; surchargeAmount?: number; totalAmount?: number; rate?: number; surchargeStatus: SurchargeStatus }> => {
+  let newRate: number | undefined = undefined;
 
+  if (request.totalAmount && request.surchargeAmount) {
+    newRate = rateCalculatorHelper(
+      undefined,
+      request.totalAmount,
+      request.surchargeAmount
+    );
+    return await returnUpdatedSurcharge(
+      request.id,
+      newRate,
+      request.totalAmount,
+      request.surchargeAmount
+    );
+  } else {
+    return await returnUpdatedSurcharge(
+      request.id,
+      undefined,
+      undefined,
+      undefined
+    );
+  }
+};
+
+async function returnUpdatedSurcharge(
+  id: string,
+  rate: number | undefined,
+  totalAmount: number | undefined,
+  surchargeAmount: number | undefined
+): Promise<{
+  id: string;
+  surchargeAmount?: number;
+  totalAmount?: number;
+  rate?: number;
+  surchargeStatus: SurchargeStatus;
+}> {
+  const updatedSurcharge = {
+    id,
+    surchargeAmount,
+    totalAmount,
+    rate,
+    surchargeStatus: SurchargeStatus.CONFIRMED,
+  };
+
+  console.log(
+    "putSurchargeUsecase surchargeAmount and totalAmount:",
+    updatedSurcharge.surchargeAmount,
+    updatedSurcharge.totalAmount
+  );
+
+  await PutSurchargeRepository(updatedSurcharge);
+  return updatedSurcharge;
 }
