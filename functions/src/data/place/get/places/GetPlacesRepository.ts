974fe04613a8e068590c22046bcfdd856e2f86e7
Bonsung Koo <developer@bonsung.me>
2025-01-22 18:36:55 +1300

    User Location is accepted in `GetPlaces` API. #21 (#22)
    close: #21

--- functions/src/data/place/get/places/GetPlacesRepository.ts
+++ functions/src/data/place/get/places/GetPlacesRepository.ts
@@ -1,8 +1,8 @@
-import { PlaceDTO } from "../place/DTO/PlaceDTO";
-import { AddressComponentsDTO } from "../place/DTO/AddressComponentsDTO";
-import { GetPlacesRepositoryResponse } from "./DTO/GetPlacesRepositoryResponse";
+import { PlaceDTO } from "../place/DTO/PlaceDTO"
+import { AddressComponentsDTO } from "../place/DTO/AddressComponentsDTO"
+import { GetPlacesRepositoryResponse } from "./DTO/GetPlacesRepositoryResponse"
 
-const locationRestrictionOfNZ = {
+export const locationRestrictionOfNZ = {
   rectangle: {
     low: {
       latitude: -47.0,
@@ -12,29 +12,42 @@ const locationRestrictionOfNZ = {
       latitude: -34.0,
       longitude: 178.0,
     },
-  },
-};
+  }
+}
 
 export async function GetPlacesRepository(
   searchText: string,
-  nextPageToken?: string
+  nextPageToken?: string,
+  userLocation?: { latitude: number, longitude: number }
 ): Promise<GetPlacesRepositoryResponse> {
-  console.log("searchFor: ", searchText);
-  console.log("nextPageToken: ", nextPageToken);
 
-  const body: string =
-    nextPageToken && nextPageToken !== ""
-      ? JSON.stringify({
-          textQuery: searchText,
-          locationRestriction: locationRestrictionOfNZ,
-          pageToken: nextPageToken,
-        })
-      : JSON.stringify({
-          textQuery: searchText,
-          locationRestriction: locationRestrictionOfNZ,
-        });
+  const reqeust = new Map<string, any>()
+
+  reqeust.set("textQuery", searchText)
+
+  if (nextPageToken) {
+    reqeust.set("pageToken", nextPageToken)
+  }
+
+  if (userLocation) {
+    reqeust.set("locationBias", {
+      "circle": {
+        "center": {
+          "latitude": userLocation.latitude,
+          "longitude": userLocation.longitude
+        },
+        "radius": 1000.0
+      }
+    })
+  } else {
+    reqeust.set("locationRestriction", locationRestrictionOfNZ)
+  }
+
+  const object = Object.fromEntries(reqeust)
+  const body: string = JSON.stringify(object)
 
   try {
+
     const response = await fetch("https://places.googleapis.com/v1/places:searchText", {
       method: "POST",
       headers: {
@@ -43,31 +56,41 @@ export async function GetPlacesRepository(
         "X-Goog-FieldMask": "nextPageToken,places.id,places.displayName,places.addressComponents",
       },
       body: body,
-    });
+    })
 
-    const data = await response.json();
-    return {
-      places: data.places.map((place: PlaceDTO) => {
-        return {
-          id: place.id,
-          displayName: {
-            text: place.displayName.text,
-            languageCode: place.displayName.languageCode,
-          },
-          addressComponents: place.addressComponents.map((component: AddressComponentsDTO) => {
-            return {
-              longText: component.longText,
-              shortText: component.shortText,
-              types: component.types,
-            };
-          }),
-          location: null,
-        };
-      }),
-      nextPageToken: data.nextPageToken,
-    };
+
+    if (response.ok) {
+
+      const data = await response.json()
+
+      return {
+        places: data.places.map((place: PlaceDTO) => {
+          return {
+            id: place.id,
+            displayName: {
+              text: place.displayName.text,
+              languageCode: place.displayName.languageCode,
+            },
+            addressComponents: place.addressComponents.map((component: AddressComponentsDTO) => {
+              return {
+                longText: component.longText,
+                shortText: component.shortText,
+                types: component.types,
+              }
+            }),
+            location: null,
+          }
+        }),
+        nextPageToken: data.nextPageToken,
+      }
+
+    }
+
+    const data = await response.json()
+
+    throw new Error(data)
+
   } catch (error) {
-    console.error("Error occurred while fetching places:", error);
-    throw error; 
+    throw error
   }
 }
