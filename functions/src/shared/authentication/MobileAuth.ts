5c17e2e1f4c296d33dc73729d1a45f77227e289c
Bonsung Koo <developer@bonsung.me>
2025-01-24 10:38:09 +1300

    Mobile authorisation component has been done. #28 (#30)
    close #28

--- functions/src/shared/authentication/MobileAuth.ts
+++ functions/src/shared/authentication/MobileAuth.ts
@@ -10,13 +10,23 @@ export const MobileAuth = async (
   const apiKey = request.headers.authorization
 
   if (!apiKey) {
-    response.status(400).send({ message: 'Bad request' })
+    response.status(401).send()
     return 
   }
 
-  if (apiKey == process.env.MOBILE_API_KEY) {
+  const [scheme, token] = apiKey.split(' ')
+
+  if (scheme !== process.env.MOBILE_API_KEY_SCHEME) {
+    response.status(401).send()
+    return
+  }
+
+  const buffer = Buffer.from(token, 'base64')
+  const decodedToken = buffer.toString('utf-8')
+
+  if (decodedToken == process.env.MOBILE_API_KEY) {
     next()
   } else {
-    response.status(401).send({ message: 'Unauthorized' })
+    response.status(403).send()
   }
 }
