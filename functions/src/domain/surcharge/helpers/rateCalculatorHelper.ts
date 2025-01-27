0b38c49ffc4168d1deec7b027c54a436a3b55fd9
PetruccioU <petr.utkin2022@gmail.com>
2025-01-28 10:06:36 +1300

    rateCalculatorHelper fixed to be applied in PutSurcharge api

--- functions/src/domain/surcharge/helpers/rateCalculatorHelper.ts
+++ functions/src/domain/surcharge/helpers/rateCalculatorHelper.ts
@@ -4,16 +4,16 @@ export function rateCalculatorHelper(
   surchargeAmount: number
 ): number { 
   if (typeof rate === 'undefined' || rate <= 0) {
-    const purchaseAmount = totalAmount - surchargeAmount;
-
+    throw new Error('Invalid input: rate amount must be greater than 0.');
+  } else {
+    if (surchargeAmount <= 0 || totalAmount <= 0) {
+      throw new Error('Invalid input: surchargeAmount and totalAmount must be greater than 0.');
+    }
+    const purchaseAmount = totalAmount - surchargeAmount
     if (purchaseAmount <= 0) {
       throw new Error('Invalid input: purchaseAmount amount must be greater than 0.');
     }
-    if (surchargeAmount <= 0) {
-      throw new Error('Invalid input: surchargeAmount amount must be greater than 0.');
-    }
-    return Math.round((surchargeAmount / purchaseAmount) * 100);
+    const rate = (surchargeAmount / purchaseAmount) * 100
+    return Number(rate.toFixed(1))
   }
-
-  return rate;
 }
