84eb166c54aacf2208b884c9b278124eccf71b47
PetruccioU <petr.utkin2022@gmail.com>
2025-01-31 14:03:22 +1300

    rateCalculatorHelper updated

--- functions/src/domain/surcharge/helpers/rateCalculatorHelper.ts
+++ functions/src/domain/surcharge/helpers/rateCalculatorHelper.ts
@@ -1,19 +1,14 @@
 export function rateCalculatorHelper(
-  rate: number | undefined, 
   totalAmount: number, 
   surchargeAmount: number
 ): number { 
-  if (typeof rate === 'undefined' || rate <= 0) {
-    throw new Error('Invalid input: rate amount must be greater than 0.');
-  } else {
-    if (surchargeAmount <= 0 || totalAmount <= 0) {
-      throw new Error('Invalid input: surchargeAmount and totalAmount must be greater than 0.');
-    }
-    const purchaseAmount = totalAmount - surchargeAmount
-    if (purchaseAmount <= 0) {
-      throw new Error('Invalid input: purchaseAmount amount must be greater than 0.');
-    }
-    const rate = (surchargeAmount / purchaseAmount) * 100
-    return Number(rate.toFixed(1))
+  if (surchargeAmount <= 0 || totalAmount <= 0) {
+    throw new Error('Invalid input: surchargeAmount and totalAmount must be greater than 0.');
   }
+  const purchaseAmount = totalAmount - surchargeAmount
+  if (purchaseAmount <= 0) {
+    throw new Error('Invalid input: purchaseAmount amount must be greater than 0.');
+  }
+  const rate = (surchargeAmount / purchaseAmount) * 100
+  return Number(rate.toFixed(1))
 }
