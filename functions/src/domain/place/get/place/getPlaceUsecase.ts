ef86e4c7161fca8330655563b96e4a418bccd9b5
PetruccioU <petr.utkin2022@gmail.com>
2025-02-04 21:57:35 +1300

    GetFranchiseRepository used in getPlaceUsecase

--- functions/src/domain/place/get/place/getPlaceUsecase.ts
+++ functions/src/domain/place/get/place/getPlaceUsecase.ts
@@ -3,11 +3,22 @@ import { GetPlaceUsecaseRequest } from "./entity/GetPlaceUsecaseRequest";
 import { GetPlaceUsecaseResponse } from "./entity/GetPlaceUsecaseResponse";
 import { GetSurchargesRepository } from '@data/surcharge'
 import { SurchargeStatus } from "@data/surcharge"
+import { GetFranchiseRepository } from "@data/franchise";
 
 export const getPlaceUsecase = async (request: GetPlaceUsecaseRequest): Promise<GetPlaceUsecaseResponse> => {
   const result = await GetPlaceRepository(request.id);
   const surchargeData = await GetSurchargesRepository(request.id);
-
+  const franchisesNames = await GetFranchiseRepository()
+  
+  let name = result.displayName.text
+  let rate = surchargeData?.rate ?? undefined
+  let status = surchargeData?.surchargeStatus ?? SurchargeStatus.UNKNOWN
+  franchisesNames.forEach(element => {
+    if(name.toLowerCase().includes(element)){
+      status = SurchargeStatus.AUTO_GENERATED
+      rate = 0.0
+    }
+  });
   return {
     id: result.id,
     displayName: {
@@ -24,8 +35,8 @@ export const getPlaceUsecase = async (request: GetPlaceUsecaseRequest): Promise<
       latitude: result.location?.latitude ?? 0,
       longitude: result.location?.longitude ?? 0,
     },
-    rate: surchargeData?.rate ?? undefined, 
+    rate: rate, 
     reportedDate: surchargeData?.reportedDate ?? undefined, // Default to 0 if reportedDate is undefined
-    surchargeStatus: surchargeData?.surchargeStatus ?? SurchargeStatus.UNKNOWN,
+    surchargeStatus: status,
   };
 };
